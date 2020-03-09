7abf77edfefd01a8af2106d21f9c552084be6a17
Marcos Daniel <marcos.mdbbr@gmail.com>
2020-03-09 05:26:26 -0300

    Input Binding

---
+++ comunication-app/src/app/input-binding/input-binding.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input-binding',
+  templateUrl: './input-binding.component.html',
+  styleUrls: ['./input-binding.component.css']
+})
+export class InputBindingComponent implements OnInit {
+
+  @Input()
+  name: string;
+  @Input('othername')
+  lastName: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
